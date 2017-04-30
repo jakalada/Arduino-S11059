632df4666be168f7539aa47d3fef0ddd4dac519f
Hideki Hamada (jakalada) <jakalada.net@gmail.com>
2017-05-01 00:30:56 +0900

    Add sensor datasheet url into comments

--- src/S11059.h
+++ src/S11059.h
@@ -1,3 +1,11 @@
+/**********************************************************************
+  Sensor Datasheet:
+    - EN: http://www.hamamatsu.com/resources/pdf/ssd/s11059-02dt_etc_kpic1082e.pdf
+          (Cat. No. KPIC1082E05 Nov. 2015 DN)
+    - JP: http://www.hamamatsu.com/resources/pdf/ssd/s11059-02dt_etc_kpic1082j.pdf
+          (Cat. No. KPIC1082J05 Nov. 2015 DN)
+**********************************************************************/
+
 #ifndef S11059_h
 #define S11059_h
 
